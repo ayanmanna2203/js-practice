4aa7e378dfeadbcc5940b384a2a84657362fc6ba
Ayan Manna <ayanmanna2203@gmail.com>
2026-08-23 19:50:01 +0530

    first commit

---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+
+// const [name , p, rating, customers, dis]=product1; // destructuring of array
+// console.log(price);
+ 
+
+
+// let {p, printproductNAme, avgRating}=product
+
+
+// console.log(p, printproductNAme(), avgRating);
