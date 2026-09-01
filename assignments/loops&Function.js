55d7ecae61424876153ce73791c159fa0559cb6a
Ayan Manna <ayanmanna2203@gmail.com>
2026-09-01 22:06:37 +0530

    assignment- loop and function

---
+++ assignments/loops&Function.js
@@ -0,0 +1,113 @@
+// for(let a=1; a<=10; a++ ){
+//     // console.log(a);
+//     // if(a%2 ==0){
+//     //     console.log(a);
+//     // }
+//     if(a%2 !=0){
+//         console.log(a);
+//     }
+// }
+// for(let a=20; a>=1; a--){
+//     console.log(a);
+// }
+
+// let sum =0;
+// for(let a=1 ; a<=10 ; a++){
+//     sum+= a;
+// }
+// console.log(sum);
+
+//multiplication table of a number
+
+// let n=9;
+// for(let a=1; a<=10; a++){
+//     let ans = n*a;
+//     console.log(n,"*",a ,"=", ans);
+// }
+
+
+// let n=1
+// let sum=0;
+// while(n<=20){
+    
+//   if(n%2 ==0){
+//       sum+=n;  
+    
+//   }
+//   n++;
+// }
+
+// console.log(sum);
+
+// let a=1;
+// while(a<10){
+//     if(a==6) break;
+//     console.log(a);
+//     a++
+// }
+
+// let a=1;
+// while(a<10){
+    
+//     console.log(a);
+//     a++;
+    
+// }
+
+// functions
+// let a;
+
+// function evenOrOdd(a){
+//     if(a%2==0){
+//         console.log("even");
+//     }
+//     else{
+//         console.log("odd");
+//     }
+
+// }
+
+// a=11;
+// evenOrOdd(a);
+
+// function largest(a, b){
+//     if(a>b){
+//         console.log(a ,"is the greater number");
+//     }
+//     else if(b>a){
+//         console.log(b," is the greater number");
+//     }
+//     else{
+//         console.log("bothe are same");
+//     }
+
+// }
+
+// let a=983265;
+// let b=983265;
+// largest(a,b)
+
+// Functions With Lopps
+
+// let printNums = function(n){
+//     let i=1;
+//     while(i<=n){
+//         console.log(i);
+//         i++
+//     }
+    
+// }
+
+// console.log(printNums(5));
+
+// let n;
+// function sum( n){
+//     let sum =0;
+//     for(let i =1; i<=n; i++){
+//         sum+=i;
+//     }
+//     return sum;
+
+// }
+// n=10;
+// console.log(sum(n));
