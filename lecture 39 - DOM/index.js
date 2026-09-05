7a201eacc41b290110483ff6cee62f42710d58a4
Ayan Manna <ayanmanna2203@gmail.com>
2026-09-05 22:53:43 +0530

    lecture - DOM

---
+++ lecture 39 - DOM/index.js
@@ -0,0 +1,75 @@
+
+// let h1 = document.querySelector("h1")
+// let h1 = document.querySelector(".h1")
+// let h1 = document.querySelector("#h1")
+
+// let h1 = document.getElementById("#h1")
+
+
+// let h1 = document.querySelector("#h1")
+// console.log(h1);
+
+// let p= document.querySelector("p")
+// p.textContent = "hello bancho kaise ho"
+// p.innerHTML = "<h2>Hello dostoo</h2>" //very very risky 
+// console.log(p); 
+
+// p.setAttribute("style", "background-color: pink ")
+
+// let btn = document.querySelector("#btn")
+
+// btn.setAttribute("disabled" , "true")
+
+// let res = btn.getAttribute("style")
+// console.log(res);
+
+
+// p.classList.add("random")
+// p.classList.remove("random")
+// p.classList.toggle("random")
+
+// p.style.backgroundColor = "blue"
+
+
+
+
+// let div = document.createElement("div")
+// let div2 = document.createElement("div")
+
+// div.textContent = "hello"
+
+
+// let body  = document.querySelector("body")
+// // body.appendChild(div)
+// // body.append(div, div2)
+
+// console.log(body);
+// let products = [
+//     {
+//         name :" iphone 18 pro",
+//         price : 47246
+//     },
+//       {
+//         name :"Samsung ",
+//         price : 83590
+//     },
+//       {
+//         name :"Realme",
+//         price : 476236
+//     },
+//       {
+//         name :" one pluse",
+//         price : 76523
+//     },
+    
+// ]
+
+
+// let productList = document.querySelector("#product-list")
+
+// products.forEach((product) =>{
+//     // console.log(product);
+//     const card = document.createElement("p")
+//     card.textContent =`${product.name} - ${product.price}`
+//     productList.append(card)
+// })
