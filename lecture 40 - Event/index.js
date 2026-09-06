f8af20b97813f4f78c35b654cbc8addf7922138e
Ayan Manna <ayanmanna2203@gmail.com>
2026-09-06 17:58:20 +0530

    even and handling

---
+++ lecture 40 - Event/index.js
@@ -0,0 +1,48 @@
+
+
+
+// let btn = document.querySelector("#gift-reveal")
+// let h1 = document.querySelector("#gift")
+// function revealgift(event){
+//     console.log("iphone 100000 pro max");
+//     // console.log(event.type);
+//     // console.log(event.target);
+//     h1.classList.toggle("reveal")
+//     // btn.classList.toggle("hidden")
+// }
+// let counter = 0
+
+// function fun1(e){
+//     if(counter < 3){
+//    console.log(e);
+//    counter++
+// }
+// else{
+//     btn.removeEventListener('click',fun1)
+// } 
+// } 
+// // btn.addEventListener(eventype , callbackfn) //syntax
+
+
+//  btn.addEventListener('click', fun1 )
+
+// let outer = document.querySelector('#outer')
+// let inner = document.querySelector('#inner')
+// let btn2 = document.querySelector('#btn2')
+
+// outer.addEventListener('click', (e)=>{
+//     e.stopPropagation()
+//     console.log("outer");
+// } )                    // capturing phase me true  {capture : true}
+// inner.addEventListener('click', (e)=>{
+//     e.stopPropagation()
+//     console.log("inner");
+// })
+// btn2.addEventListener('click', (e)=>{
+//     e.stopPropagation()
+   
+//     console.log("btn2");
+// })
+   
+// closest concept
+
