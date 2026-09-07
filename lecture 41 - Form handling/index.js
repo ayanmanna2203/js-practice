89d1ed4e9f3fcaf01edf0c4b32db4dd765f92853
Ayan Manna <ayanmanna2203@gmail.com>
2026-09-07 11:25:43 +0530

    complete lect 41

--- lecture 41 - Form handling/index.js
+++ lecture 41 - Form handling/index.js
@@ -6,8 +6,9 @@ const country = document.querySelector("#Country")
 const checkbox = document.querySelector("#checkbox")
 const pashint = document.querySelector("#hint")
 const password = document.querySelector("#pass")
-const errormassage = document.querySelector("#errormassage")
-
+const errormassage = document.querySelector(".errormassage")
+const mail = document.querySelector("#email")
+const successmsg = document.querySelector("#successmsg")
 // function isValidUsername(username){
 //      if(username.value.trim().length=== 0){
 //         errormassage.textContent = "enter your name"
@@ -18,20 +19,86 @@ const errormassage = document.querySelector("#errormassage")
 
 // } 
 
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const mail = document.querySelector("#email").value
-    // const password = document.querySelector("#pass").value
+function showerror(input, errormassage){
+    input.parentElement.querySelector(".errormassage").textContent = errormassage
+}
 
-    if(username.value.trim().length=== 0){
-        console.log("Enter  Your Name");
-        // alert("please enter your name")
-        errormassage.textContent = "please enter your name"
-        return
+function clearError(input){
+    input.parentElement.querySelector(".errormassage").textContent = ""
+}
+
+
+function validUsername(username){
+    if(username.value.trim().length === 0){
+        showerror(username , "Please Enter your Name")
+        return false
+    }
+    if(username.value.trim().length < 3){
+        showerror(username , "username must be at least 3 character")
+        return false
     }
 
-    console.log({ username: username.value, password, mail });
+    clearError(username)
+    return true
+}
+
+function validPassword(password){
+    if(password.value.trim().length === 0){
+        showerror(password , "Please Enter your password")
+        return false
+    }
+    if(password.value.trim().length < 8){
+        showerror(password , "password must be atleast 8 character")
+        return false
+    }
+
+    clearError(password)
+    return true
+}
+
+
+function vaildEmail(mail){
+    if(mail.include('@') ){
+        clearError(mail)
+    }
+    else{
+        showerror(mail , "Enter a valid mail id")
+    }
+
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    // const password = document.querySelector("#pass").value
+
+    const isUserValid = validUsername(username);
+    const ispassValid = validPassword(password)
+    // const ismailValid = vaildEmail(mail);
+    // if(ismailValid == true){
+    //     console.log("form isvalid");
+    // }
+    // else{
+    //     console.log("form invalid");
+    // }
+
+    // if(username.value.trim().length=== 0){
+    //     console.log("Enter  Your Name");
+    //     // alert("please enter your name")
+    //     errormassage.textContent = "please enter your name"
+    //     return
+    // }
+    if(isUserValid === true && ispassValid ===true){
+        successmsg.classList.remove("hidden")
+    }
+    else{
+        successmsg.classList.add("hidden")
+    }
+
+    console.log({ username: username.value, password: password.value, mail: mail.value });
 })
+
+
 
 bio.addEventListener("input",(e) =>{
     const remaining =150 - bio.value.length;
