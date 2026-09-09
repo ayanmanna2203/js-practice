6ccf0296c0a1caf3a983d46beb2bf7102c8c22a2
Ayan Manna <ayanmanna2203@gmail.com>
2026-09-09 22:02:11 +0530

    DOM part - 1

---
+++ assignments/DOM.js
@@ -0,0 +1,20 @@
+
+
+let title = document.getElementById("title")
+title.textContent ="Hello Javascript"
+
+let description = document.querySelector("#description")
+description.textContent="New Description"
+
+let items= document.querySelector(".item")
+
+items.classList.add("changeColor")
+//   for (let i = 0; i < items.length; i++) {
+//         items[i].classList.add(".changeColor");
+//     }
+// console.log();
+
+
+
+
+
